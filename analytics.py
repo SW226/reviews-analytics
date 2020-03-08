d7cd28a234a648a95b1d7a3ffdbab267cb418764
SW <hsuapang81@gmail.com>
2020-03-08 16:02:23 +0800

    add word count function

--- analytics.py
+++ analytics.py
@@ -1,11 +1,41 @@
 data = []
+count = 0
 with open ('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-print(len(data))
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完了,總共有', len(data), '筆資料')
 print(data[0])
+
 
 sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言的平均長度是', sum_len/len(data))
+
+
+#文字計數
+wc = {}	# word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word,wc[word])
+	
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為: ', wc[word])
+	else:
+		print('這個字沒有出現過')
+print('感謝使用本查詢功能')
